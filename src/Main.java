30148487165610cfe5aaa11fdec3bb2ae04d25f5
doom <de.simone.dc@gmail.com>
2025-08-31 21:25:44 +0200

    ajout de la class Contact et test dans main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import model.Contact;
+
 public class Main {
     public static void main(String[] args) {
+        Contact contact1 = new Contact(00001, "Robert Duchemin", "0470/01.02.03", "duchemin.r@hotmail.com");
 
+        System.out.println(contact1);
     }
 }
